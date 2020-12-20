b52c9810788b1c8ee4db7210b1abf37c417c1b65
d02d33pak <deepak030190@gmail.com>
2020-12-20 16:44:10 +0530

    day 04 part 2 completed

--- day_04/day04.py
+++ day_04/day04.py
@@ -2,6 +2,7 @@
 Advent of Code : Day 04
 """
 
+import re
 from os import path
 
 
@@ -46,6 +47,7 @@ def part1(list_of_passports):
         for field in req_fields:
             if field not in passport:
                 valid = False
+
         if valid:
             valid_count += 1
 
@@ -55,4 +57,74 @@ def part1(list_of_passports):
 # PART 2
 def part2(list_of_passports):
     """ Find valid passports based on stricter criteria """
-    return 0
+    valid_count = 0
+    req_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    year_pattern = re.compile(r"^(\d{4})$")
+    hgt_pattern = re.compile(r"^(\d+)(cm|in)$")
+    hcl_pattern = re.compile(r"^#[0-9a-f]{6}$")
+    ecl_pattern = re.compile(r"^(amb|blu|brn|gry|grn|hzl|oth)$")
+    pid_pattern = re.compile(r"^(\d{9})$")
+
+    for passport in list_of_passports:
+        valid = True
+        for field in req_fields:
+            if field in passport:
+                if field == "byr":
+                    match = year_pattern.match(passport[field])
+                    if match:
+                        if int(match.group(1)) not in range(1920, 2002 + 1):
+                            valid = False
+                    else:
+                        valid = False
+
+                elif field == "iyr":
+                    match = year_pattern.match(passport[field])
+                    if match:
+                        if int(match.group(1)) not in range(2010, 2020 + 1):
+                            valid = False
+                    else:
+                        valid = False
+
+                elif field == "eyr":
+                    match = year_pattern.match(passport[field])
+                    if match:
+                        if int(match.group(1)) not in range(2020, 2030 + 1):
+                            valid = False
+                    else:
+                        valid = False
+
+                elif field == "hgt":
+                    match = hgt_pattern.match(passport[field])
+                    if match:
+                        if match.group(2) == "cm":
+                            if int(match.group(1)) not in range(150, 193 + 1):
+                                valid = False
+                        elif match.group(2) == "in":
+                            if int(match.group(1)) not in range(59, 76 + 1):
+                                valid = False
+                        else:
+                            valid = False
+                    else:
+                        valid = False
+
+                elif field == "hcl":
+                    match = hcl_pattern.match(passport[field])
+                    if not match:
+                        valid = False
+
+                elif field == "ecl":
+                    match = ecl_pattern.match(passport[field])
+                    if not match:
+                        valid = False
+
+                elif field == "pid":
+                    match = pid_pattern.match(passport[field])
+                    if not match:
+                        valid = False
+            else:
+                valid = False
+
+        if valid:
+            valid_count += 1
+
+    return valid_count
