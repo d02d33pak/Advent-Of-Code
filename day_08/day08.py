b7dfb0e4b0dea541b93091f0eec2e305669bc6f7
d02d33pak <deepak030190@gmail.com>
2020-12-23 20:23:35 +0530

    day 8 completed

--- day_08/day08.py
+++ day_08/day08.py
@@ -23,21 +23,60 @@ def part1(instructions):
     accumulator = 0
     visited = set()
 
-    while index not in visited:
+    while index not in visited and index < len(instructions):
         visited.add(index)
         opr, val = instructions[index].split()
-        val = int(val)
 
-        index = index + val if opr == "jmp" else index + 1
+        index = index + int(val) if opr == "jmp" else index + 1
 
         if opr == "acc":
-            accumulator += val
+            accumulator += int(val)
 
     return accumulator
+
+
+def not_infinite(instructions):
+    """ check if loop is infinite """
+    index = 0
+    visited = set()
+
+    while (index not in visited) and (index < len(instructions)):
+        visited.add(index)
+        opr, val = instructions[index].split()
+        index = index + int(val) if opr == "jmp" else index + 1
+
+    if index == len(instructions):
+        return True
+
+    return False
 
 
 # PART 2
 def part2(instructions):
     """ Find nop, jmp to be swapped """
-    print(instructions)
-    return 0
+    index = 0
+    accumulator = 0
+    while index < len(instructions):
+
+        found = False
+
+        instruction = instructions[index]
+        opr, val = instruction.split()
+
+        if opr == "nop":
+            new_instructions = instructions.copy()
+            new_instructions[index] = "jmp " + val
+            found = True
+        elif opr == "jmp":
+            new_instructions = instructions.copy()
+            new_instructions[index] = "nop " + val
+            found = True
+
+        if found:
+            if not_infinite(new_instructions):
+                accumulator = part1(new_instructions)
+                break
+
+        index += 1
+
+    return accumulator
