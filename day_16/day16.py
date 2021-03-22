d77903975c20225332e595c0cf0de43b4fbd193e
d02d33pak <deepak030190@gmail.com>
2021-03-23 00:49:32 +0530

    day 16 part 2 done

--- day_16/day16.py
+++ day_16/day16.py
@@ -33,6 +33,32 @@ def parse_input(filename):
     return [attributes, tickets]
 
 
+def is_ticket_valid(ticket, attr):
+    """
+    Util fn to check if ticket is valid
+    based on given attribute's ranges
+    """
+    sum_invalid = 0
+    ticket_valid = True
+
+    for value in ticket:
+        is_invalid = True
+
+        for _, ranges in attr.items():
+            low1, high1 = ranges[0][0], ranges[0][1] + 1
+            low2, high2 = ranges[1][0], ranges[1][1] + 1
+
+            if value in range(low1, high1) or value in range(low2, high2):
+                is_invalid = False
+                break
+
+        if is_invalid:
+            sum_invalid += value
+            ticket_valid = False
+
+    return ticket_valid, sum_invalid
+
+
 # ----------------------------------------------
 #  Data is split in 2 parts:
 #  0. Dictionary of attributes and their ranges
@@ -41,7 +67,7 @@ def parse_input(filename):
 
 # PART 1
 def part1(data):
-    """ Check Valid Passwords """
+    """ Sum on invalid values in nearby tickets """
 
     attr = data[0]
     tickets = data[1]
@@ -49,19 +75,8 @@ def part1(data):
     sum_invalid = 0
 
     for ticket in tickets:
-        for value in ticket:
-            is_invalid = True
-
-            for _, ranges in attr.items():
-                low1, high1 = ranges[0][0], ranges[0][1] + 1
-                low2, high2 = ranges[1][0], ranges[1][1] + 1
-
-                if value in range(low1, high1) or value in range(low2, high2):
-                    is_invalid = False
-                    break
-
-            if is_invalid:
-                sum_invalid += value
+        _, sum_per_ticket = is_ticket_valid(ticket, attr)
+        sum_invalid += sum_per_ticket
 
     return sum_invalid
 
@@ -70,6 +85,54 @@ def part1(data):
 def part2(data):
     """ Check on other criteria """
 
-    print(data)
+    attr = data[0]
+    tickets = data[1]
 
-    return 0
+    valid_tickets = []
+
+    for ticket in tickets:
+        valid_ticket, _ = is_ticket_valid(ticket, attr)
+        if valid_ticket:
+            valid_tickets.append(ticket)
+
+    possible_col = {}  # dict to hold possible col for each field/attr/rule
+
+    for field, ranges in attr.items():
+        low1, high1 = ranges[0][0], ranges[0][1] + 1
+        low2, high2 = ranges[1][0], ranges[1][1] + 1
+        valid_col = []
+
+        for i in range(20):
+            all_pass = True
+
+            for ticket in valid_tickets:
+                value = ticket[i]
+
+                if value not in range(low1, high1) and value not in range(low2, high2):
+                    all_pass = False
+                    break
+
+            if all_pass:
+                valid_col.append(i)
+
+        possible_col[field] = valid_col
+
+    fixed_col = {}  # dict to hold confirmed col for each field/attr/rule
+
+    while len(fixed_col) < 20:
+        fixed_field = {
+            key: value[0] for key, value in possible_col.items() if len(value) == 1
+        }
+        [(key, value)] = fixed_field.items()  # only contain 1 key-pair value
+        fixed_col[key] = value
+
+        for value_list in possible_col.values():
+            if value in value_list:
+                value_list.remove(value)
+
+    product_of_departure = 1
+    for key, value in fixed_col.items():
+        if key.startswith("departure"):
+            product_of_departure *= valid_tickets[0][value]
+
+    return product_of_departure
