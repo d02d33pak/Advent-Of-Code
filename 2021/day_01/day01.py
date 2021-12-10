1c8491ad8581f172ee914d2d8d2eaa3914ee91e1
d02d33pak <deepak030190@gmail.com>
2021-12-10 09:15:46 +0530

    day1 done

--- 2021/day_01/day01.py
+++ 2021/day_01/day01.py
@@ -11,17 +11,25 @@ def parse_input(filename):
     file_path = path.join(script_dir, filename)
 
     with open(file_path, "r", encoding="utf-8") as file:
-        values = set(map(int, file.read().splitlines()))
+        values = list(map(int, file.read().splitlines()))
     return values
 
 
 # PART 1
-def part1():
-    """Part 1"""
-    return True
+def part1(depths):
+    """Count no. of times depth increases"""
+    increase_count = 0
+    for i in range(1, len(depths)):
+        if depths[i] > depths[i - 1]:
+            increase_count += 1
+    return increase_count
 
 
 # PART 2
-def part2():
-    """Part 2"""
-    return True
+def part2(depths):
+    """Count no. of times sum of depths increases"""
+    three_measurement_sum = []
+    for i in range(len(depths) - 2):
+        three_measurement_sum.append(sum(depths[i : i + 3]))
+
+    return part1(three_measurement_sum)
--- 2021/day_01/test_day01.py
+++ 2021/day_01/test_day01.py
@@ -7,9 +7,9 @@ from day01 import parse_input, part1, part2
 
 def test_part1():
     """ Checks for nums in list """
-    assert part1(parse_input("test_input")) == True
+    assert part1(parse_input("test_input")) == 7
 
 
 def test_part2():
     """ Test part two """
-    assert part2(parse_input("test_input")) == True
+    assert part2(parse_input("test_input")) == 5
